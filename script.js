2d645d034d6be867caa3e7bf0f300344018336b0
KaunAnkit <kaunankitjha@gmail.com>
2025-04-29 21:00:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+function playSpotify() {
+    const linkInput = document.getElementById("spotifyLink").value.trim();
+    const playerContainer = document.getElementById("playerContainer");
+
+    
+    let embedUrl = "";
+
+    if (linkInput.includes("track")) {
+        const trackId = linkInput.split("track/")[1].split("?")[0];
+        embedUrl = `https://open.spotify.com/embed/track/${trackId}`;
+    } else if (linkInput.includes("playlist")) {
+        const playlistId = linkInput.split("playlist/")[1].split("?")[0];
+        embedUrl = `https://open.spotify.com/embed/playlist/${playlistId}`;
+    } else {
+        playerContainer.innerHTML = "Invalid Spotify link.";
+        return;
+    }
+
+    
+    playerContainer.innerHTML = `
+        <iframe style="border-radius:12px" 
+                src="${embedUrl}" 
+                width="500" 
+                height="80" 
+                frameBorder="0" 
+                allowtransparency="true" 
+                allow="encrypted-media">
+        </iframe>
+    `;
+}
+
+
